0ce40a9265696fba81678e02826e337e0c26b58e
Abdulla Shahil <abdullashahil0077@gmail.com>
2025-02-26 14:40:02 +0530

    Winston Loggers

--- Server/src/logger.js
+++ Server/src/logger.js
@@ -1,23 +1,23 @@
 import winston from "winston";
-import DailyRotateFile from "winston-daily-rotate-file";
-import fs from "fs";
-import path from "path";
+// import DailyRotateFile from "winston-daily-rotate-file";
+// import fs from "fs";
+// import path from "path";
 import dotenv from "dotenv";
-import { fileURLToPath } from "url";
+// import { fileURLToPath } from "url";
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
+// const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 
 // Log directory setup
-const logDirectory = process.env.LOG_DIR || "/var/data";
-if (!fs.existsSync(logDirectory)) {
-    fs.mkdirSync(logDirectory, { recursive: true });
-}
+// const logDirectory = process.env.LOG_DIR || path.join(__dirname, "..", "logs");
+// if (!fs.existsSync(logDirectory)) {
+//     fs.mkdirSync(logDirectory, { recursive: true });
+// }
 
 const uppercaseLevel = winston.format((info) => {
-    info.level = info.level.toUpperCase();
+    info.level = info.level.toUpperCase(); // Convert level to uppercase
     return info;
 });
 
@@ -27,17 +27,17 @@ const logFormat = winston.format.printf(({ level, message, timestamp, functionNa
 });
 
 // Log transport with rotation
-const rotateFileTransport = new DailyRotateFile({
-    filename: path.join(logDirectory, "app-%DATE%.log"), //(e.g., app-2025-02-26.log)
-    datePattern: "YYYY-MM-DD",
-    maxSize: "10m",
-    maxFiles: "14d",
-    format: winston.format.combine(
-        uppercaseLevel(), // Capitalize log levels
-        winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-        winston.format.json()
-    ),
-});
+// const rotateFileTransport = new DailyRotateFile({
+//     filename: path.join(logDirectory, "app-%DATE%.log"), //(e.g., app-2025-02-26.log)
+//     datePattern: "YYYY-MM-DD",
+//     maxSize: "10m",
+//     maxFiles: "14d",
+//     format: winston.format.combine(
+//         uppercaseLevel(), // Capitalize log levels
+//         winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+//         winston.format.json()
+//     ),
+// });
 
 // Console format
 const consoleFormat = winston.format.combine(
@@ -52,7 +52,7 @@ const logger = winston.createLogger({
     level: "info",
     transports: [
         new winston.transports.Console({ format: consoleFormat }),
-        rotateFileTransport
+        // rotateFileTransport
     ],
 });
 
