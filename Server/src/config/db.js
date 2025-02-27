ec2396662c96b8fa0a895a563a21b77002f51167
Abdulla Shahil <abdullashahil0077@gmail.com>
2025-02-27 09:52:01 +0530

    Production

--- Server/src/config/db.js
+++ Server/src/config/db.js
@@ -18,9 +18,9 @@ const db = new pg.Client({
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+    // ssl: {
+    //     rejectUnauthorized: false,
+    // },
 });
 
 db.connect()
