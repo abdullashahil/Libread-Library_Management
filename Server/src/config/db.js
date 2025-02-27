19a83e91977e72ecf35b313f2d08bb8a2441d47d
Abdulla Shahil <abdullashahil0077@gmail.com>
2025-02-27 10:32:01 +0530

    Production

--- Server/src/config/db.js
+++ Server/src/config/db.js
@@ -4,25 +4,11 @@ import { logMessage } from "../logger.js";
 
 dotenv.config();
 
-// const db = new pg.Client({
-//   user: process.env.PG_USER,
-//   host: process.env.PG_HOST,
-//   database: process.env.PG_DATABASE,
-//   password: process.env.PG_PASSWORD,
-//   port: process.env.PG_PORT,
-// });
-
 const db = new pg.Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-    ssl: false
-    // ssl: {
-    //     rejectUnauthorized: false,
-    // },
+    connectionString: process.env.DATABASE_URL,
+
 });
+
 
 db.connect()
     .then(() => logMessage("info", "database", "Connected to Aiven PostgreSQL"))
