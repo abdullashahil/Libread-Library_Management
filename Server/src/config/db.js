7fd56dbcd480e54786ebe9ad641047b14bf225d7
Abdulla Shahil <abdullashahil0077@gmail.com>
2025-02-27 11:53:06 +0530

    Production

--- Server/src/config/db.js
+++ Server/src/config/db.js
@@ -6,10 +6,6 @@ dotenv.config();
 
 const db = new pg.Client({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-    ssl: false
 });
 
 db.connect()
